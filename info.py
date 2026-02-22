73b5e3302983b72e2dc0298344cb0a93bc95ad40
gaveranubhav-cpu <gaveranubhav@gmail.com>
2026-02-22 17:19:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -130,13 +130,13 @@ SEASONS = [f"season {i}" for i in range(1, 23)]
 START_IMG = (
     environ.get(
         "START_IMG",
-        "https://i.ibb.co/qpxpGmC/image.jpg https://i.ibb.co/DQ35zLZ/image.jpg",
+        "https://i.ibb.co/qMJWYyWc/photo-2026-02-22-11-37-16-7609654825478782992.jpg",
     )
 ).split()
 FORCESUB_IMG = environ.get("FORCESUB_IMG", "https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg")
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split()
 PAYPICS = (
-    environ.get("PAYPICS", "https://graph.org/file/f4db1c3ad3d9e38b328e6.jpg")
+    environ.get("PAYPICS", "https://i.ibb.co/S4ZQHdss/photo-2026-02-22-10-53-19-7609641317806637076.jpg")
 ).split()
 SUBSCRIPTION = environ.get(
     "SUBSCRIPTION", "https://graph.org/file/9f3f47c690bbcc67633c2.jpg"
